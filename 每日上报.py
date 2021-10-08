5ae03fa6837e4089595ec31c469dd3e4aa659c67
火炉旁的啸尘 <47934008+xiaochen1111@users.noreply.github.com>
2021-10-08 10:46:17 +0800

    Add files via upload

--- 每日上报.py
+++ 每日上报.py
@@ -27,8 +27,10 @@ for i in range (0, 5):
 		driver.execute_script(f'kzl10 = "{LOCATION}"')
 		driver.execute_script('document.getElementById("kzl18-0").checked = true')
 		driver.execute_script('document.getElementById("kzl32-0").checked = true')
+        driver.execute_script('document.getElementById("kzl41-0").checked = true')
 		driver.execute_script('document.getElementById("txfscheckbox").click()')
 		driver.execute_script('document.getElementById("txfscheckbox1").click()')
+        driver.execute_script('document.getElementById("txfscheckbox2").click()')
 		driver.find_element_by_class_name('submit').click()
 		success = True
 		break
