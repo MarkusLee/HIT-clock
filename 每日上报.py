8df9df3cadc9d74ea255c287beb22482b36cd3e0
火炉旁的啸尘 <47934008+xiaochen1111@users.noreply.github.com>
2021-11-19 15:59:57 +0800

    Add files via upload

---
+++ 每日上报.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import os
+import traceback
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+
+print('初始化浏览器')
+USERNAME   = ['19B905030']
+PASSWORD   = ['15849447750jxzs']
+LOCATION   = ['黑龙江省哈尔滨市南岗区']
+ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Mobile/14A403 NetType/WIFI Language/zh_CN'
+app = 'HuaWei-AnyOffice/1.0.0/cn.edu.hit.welink'
+option = webdriver.ChromeOptions()
+option.headless = True
+option.add_argument('user-agent='+ua)
+driver = webdriver.Chrome(executable_path= '/usr/bin/chromedriver', options = option)
+
+print('正在上报')
+driver.get('https://ids.hit.edu.cn/authserver/')
+driver.find_element_by_id('mobileUsername').send_keys(USERNAME)
+driver.find_element_by_id('mobilePassword').send_keys(PASSWORD)
+driver.find_element_by_id('load').click()
+driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": ua + ' ' + app})
+
+s = 'kzl10 = '
+
+success = False
+for i in range (0, 5):
+	try:
+		driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew/index')
+		driver.execute_script(f'kzl10 = "{LOCATION}"') 
+		driver.execute_script('document.getElementById("kzl18-0").checked = true')
+		driver.execute_script('document.getElementById("kzl32-0").checked = true')
+		driver.execute_script('document.getElementById("kzl41-0").checked = true')
+		driver.execute_script('document.getElementById("txfscheckbox").click()')
+		driver.find_element_by_class_name('submit').click()
+		success = True
+		break
+	except:
+		traceback.print_exc()
+		print('失败' + str(i+1) + '次，正在重试...')
+driver.quit()
+if success:
+	print('上报完成')
+else:
+	raise Exception('上报多次失败，可能学工系统已更新')
